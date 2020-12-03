082289bbadddde723c8f216cc6c9454c90c932e1
Johannes Huwald <johannes.huwald@gmail.com>
2020-12-03 17:45:11 +0100

    Add a simple org file for testing and an entry class for the parser
    The parser is going to work on a directory and will parse all files the
    directory contains. For now, it only creates an empty class instance for
    each file

---
+++ worktree.rb
@@ -0,0 +1,32 @@
+class OrgParseError < ::StandardError
+end
+
+class OrgReadFileError < OrgParseError
+end
+
+class OrgDirectory
+  def initialize dirname
+    @name = dirname
+    @files = []
+    raise OrgReadFileError, "Path given for OrgDirectory is no directory" unless Dir.exist? @name
+
+    @files = Dir.glob("**/*.org", base: @name).map { |f| OrgFile.new @name, f }
+
+    @files.each { |f| puts f.path }
+  end
+end
+
+class OrgFile
+  def initialize dirname=nil, file_name
+    @name = (dirname == nil) ? file_name : File.join(dirname, file_name)
+    raise OrgReadFileError, "file '#{@name}' does not exist" unless File.file? @name
+
+  end
+
+  def path
+    @name
+  end
+end
+
+#OrgDirectory.new "test"
+OrgFile.new "test/simple.org"

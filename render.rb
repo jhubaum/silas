f3fc32ab10b59eaf6e45bc576e947f1787c0c187
Johannes Huwald <johannes.huwald@gmail.com>
2020-12-13 13:43:15 +0100

    Add website header
    For now it's my name and four hardcoded links that will be changed in a
    later version.

--- render.rb
+++ render.rb
@@ -5,7 +5,13 @@ module SSG
   def render_post file, path
     template = Tilt.new('templates/post.html.erb')
     File.open(path + "/index.html", "w+") do |f|
-      f.write template.render(file, :title => "Example post")
+      f.write template.render(file, :title => "Example post",
+                              :pages => [
+                                ["https://jhuwald.com/about/", "About"],
+                                ["https://jhuwald.com/blog/", "Blog"],
+                                ["https://jhuwald.com/projects/", "Projects"],
+                                ["https://jhuwald.com/stories/", "Stories"]
+                              ])
     end
   end
 end

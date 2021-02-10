5bcf88d6161a3144dc0a8f62efa2fa8d3801c990
Johannes Huwald <johannes.huwald@gmail.com>
2021-02-10 19:46:28 +0100

    Add argparser using clap
    Add arguments to support either generating all files or a single file.
    Generating a single is not yet implemented and will be added in a later
    commit.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use std::env::args;
+use clap;
 
 mod builder;
 use builder::Builder;
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    let mut builder = match Builder::new(&args[1]) {
-        Ok(builder) => builder,
-        Err(error) => panic!("Couldn't load builder: {:?}", error)
-    };
+    let matches = clap::App::new("Silas")
+        .version("0.1")
+        .author("Johannes Huwald <hey@jhuwald.com>")
+        .about("The SSG for my blog at jhuwald.com")
+        .arg(clap::Arg::with_name("PATH")
+             .help("The path to the blog folder, or, if --file is set, to the file")
+             .required(true))
+        .arg(clap::Arg::with_name("file")
+             .long("file")
+             .short("f")
+             .help("Generate the html output for a single org file")
+             .required(false)
+             .takes_value(false))
+        .get_matches();
 
-    if let Err(error) = builder.generate("generated", true) {
-        panic!("Couldn't generate site: {:?}", error);
+    if matches.is_present("file") {
+        println!("Loading file {}", matches.value_of("PATH").unwrap());
     } else {
-        println!("Generation successful!");
+        let mut builder = match Builder::new(matches.value_of("PATH").unwrap()) {
+            Ok(builder) => builder,
+            Err(error) => panic!("Couldn't load builder: {:?}", error)
+        };
+
+        if let Err(error) = builder.generate("generated", true) {
+            panic!("Couldn't generate site: {:?}", error);
+        } else {
+            println!("Generation successful!");
+        }
     }
 }
